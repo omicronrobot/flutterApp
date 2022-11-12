200116fd48289d92184bb271ba3ebece3e12f2ea
Kelvin Gitu <gitukelvin01@gmail.com>
2022-11-12 15:39:27 +0300

    cleanup

--- lib/pages/setup.dart
+++ lib/pages/setup.dart
@@ -50,20 +50,16 @@ class _SetupPageState extends State<SetupPage> {
     double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,   //new line
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: Center(
         child: Column(
           children: [
             Center(
                 child: CustomPaint(
-              // TODO Return to 0.4
               size: Size(width, height * 0.3),
               painter: CurvedPainter(),
             )),
-            // const SizedBox(
-            //   height: 2,
-            // ),
             Text(
               'Welcome to Omicron',
               style: TextStyle(
@@ -82,207 +78,205 @@ class _SetupPageState extends State<SetupPage> {
               child: Container(
                 alignment: Alignment.center,
                 padding: const EdgeInsets.symmetric(horizontal: 23.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        child: DropdownButtonFormField(
-                          value: _protocol,
-                          icon: const Icon(
-                            Icons.keyboard_arrow_down,
-                          ),
-                          items: protocolItems.map((String items) {
-                            return DropdownMenuItem(
-                              value: items,
-                              child: Text(items),
-                            );
-                          }).toList(),
-                          onChanged: (newValue) {
-                            setState(() {
-                              _protocol = newValue.toString();
-                            });
-                          },
-                          style: TextStyle(
-                            fontFamily: 'FredokaOne',
-                            fontSize: 15,
-                            color: Colors.orange[700],
-                          ),
-                          decoration: const InputDecoration(
-                            labelText: 'Enter Protocol',
-                          ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: DropdownButtonFormField(
+                        value: _protocol,
+                        icon: const Icon(
+                          Icons.keyboard_arrow_down,
+                        ),
+                        items: protocolItems.map((String items) {
+                          return DropdownMenuItem(
+                            value: items,
+                            child: Text(items),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            _protocol = newValue.toString();
+                          });
+                        },
+                        style: TextStyle(
+                          fontFamily: 'FredokaOne',
+                          fontSize: 15,
+                          color: Colors.orange[700],
+                        ),
+                        decoration: const InputDecoration(
+                          labelText: 'Enter Protocol',
                         ),
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        child: TextFormField(
-                          keyboardType: TextInputType.url,
-                          style: TextStyle(
-                            fontFamily: 'FredokaOne',
-                            fontSize: 15,
-                            color: Colors.orange[700],
-                          ),
-                          textAlign: TextAlign.start,
-                          decoration: const InputDecoration(
-                            labelText: 'Enter URI Endpoint',
-                            hintText: 'http://omicron.rodneyosodo.com/api',
-                            labelStyle: TextStyle(),
-                            hintStyle: TextStyle(),
-                          ),
-                          onChanged: (value) {
-                            _endpoint = value;
-                          },
-                          validator: (value) {
-                            if (_protocol == "http") {
-                              if (isURL(value)) {
-                                return null;
-                              } else {
-                                return "Enter Endpoint URI";
-                              }
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: TextFormField(
+                        keyboardType: TextInputType.url,
+                        style: TextStyle(
+                          fontFamily: 'FredokaOne',
+                          fontSize: 15,
+                          color: Colors.orange[700],
+                        ),
+                        textAlign: TextAlign.start,
+                        decoration: const InputDecoration(
+                          labelText: 'Enter URI Endpoint',
+                          hintText: 'http://omicron.rodneyosodo.com/api',
+                          labelStyle: TextStyle(),
+                          hintStyle: TextStyle(),
+                        ),
+                        onChanged: (value) {
+                          _endpoint = value;
+                        },
+                        validator: (value) {
+                          if (_protocol == "http") {
+                            if (isURL(value)) {
+                              return null;
+                            } else {
+                              return "Enter Endpoint URI";
                             }
-                            return null;
-                          },
+                          }
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: TextFormField(
+                        keyboardType: TextInputType.url,
+                        style: TextStyle(
+                          fontFamily: 'FredokaOne',
+                          fontSize: 15,
+                          color: Colors.orange[700],
                         ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        child: TextFormField(
-                          keyboardType: TextInputType.url,
-                          style: TextStyle(
-                            fontFamily: 'FredokaOne',
-                            fontSize: 15,
-                            color: Colors.orange[700],
-                          ),
-                          textAlign: TextAlign.start,
-                          decoration: const InputDecoration(
-                            labelText: 'Enter MQTT Username',
-                            hintText: 'cfc7a6c8-941e-4d72-be17-dbcc24b17fa3',
-                            labelStyle: TextStyle(),
-                            hintStyle: TextStyle(),
-                          ),
-                          onChanged: (value) {
-                            _mqttUsername = value;
-                          },
+                        textAlign: TextAlign.start,
+                        decoration: const InputDecoration(
+                          labelText: 'Enter MQTT Username',
+                          hintText: 'cfc7a6c8-941e-4d72-be17-dbcc24b17fa3',
+                          labelStyle: TextStyle(),
+                          hintStyle: TextStyle(),
                         ),
+                        onChanged: (value) {
+                          _mqttUsername = value;
+                        },
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        child: TextFormField(
-                          keyboardType: TextInputType.url,
-                          style: TextStyle(
-                            fontFamily: 'FredokaOne',
-                            fontSize: 15,
-                            color: Colors.orange[700],
-                          ),
-                          textAlign: TextAlign.start,
-                          decoration: const InputDecoration(
-                            labelText: 'Enter MQTT Password',
-                            hintText: '7686eb82-d6b5-4513-b559-d867c8ed85e3',
-                            labelStyle: TextStyle(),
-                            hintStyle: TextStyle(),
-                          ),
-                          onChanged: (value) {
-                            _mqttPassword = value;
-                          },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: TextFormField(
+                        keyboardType: TextInputType.url,
+                        style: TextStyle(
+                          fontFamily: 'FredokaOne',
+                          fontSize: 15,
+                          color: Colors.orange[700],
                         ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        child: TextFormField(
-                          keyboardType: TextInputType.url,
-                          style: TextStyle(
-                            fontFamily: 'FredokaOne',
-                            fontSize: 15,
-                            color: Colors.orange[700],
-                          ),
-                          textAlign: TextAlign.start,
-                          decoration: const InputDecoration(
-                            labelText: 'Enter Publisher Channel',
-                            hintText:
-                                'channels/fadfba2a-297c-49ef-87f3-c3b4745ce2ec/messages',
-                            labelStyle: TextStyle(),
-                            hintStyle: TextStyle(),
-                          ),
-                          onChanged: (value) {
-                            _pubChannel = value;
-                          },
+                        textAlign: TextAlign.start,
+                        decoration: const InputDecoration(
+                          labelText: 'Enter MQTT Password',
+                          hintText: '7686eb82-d6b5-4513-b559-d867c8ed85e3',
+                          labelStyle: TextStyle(),
+                          hintStyle: TextStyle(),
                         ),
+                        onChanged: (value) {
+                          _mqttPassword = value;
+                        },
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        child: TextFormField(
-                          keyboardType: TextInputType.url,
-                          style: TextStyle(
-                            fontFamily: 'FredokaOne',
-                            fontSize: 15,
-                            color: Colors.orange[700],
-                          ),
-                          textAlign: TextAlign.start,
-                          decoration: const InputDecoration(
-                            labelText: 'Enter HTTP Auth',
-                            hintText: 'Thing 7686eb82-d6b5-4513-b559-d867c8ed85e3',
-                            labelStyle: TextStyle(),
-                            hintStyle: TextStyle(),
-                          ),
-                          onChanged: (value) {
-                            _httpAuth = value;
-                          },
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: TextFormField(
+                        keyboardType: TextInputType.url,
+                        style: TextStyle(
+                          fontFamily: 'FredokaOne',
+                          fontSize: 15,
+                          color: Colors.orange[700],
                         ),
+                        textAlign: TextAlign.start,
+                        decoration: const InputDecoration(
+                          labelText: 'Enter Publisher Channel',
+                          hintText:
+                              'channels/fadfba2a-297c-49ef-87f3-c3b4745ce2ec/messages',
+                          labelStyle: TextStyle(),
+                          hintStyle: TextStyle(),
+                        ),
+                        onChanged: (value) {
+                          _pubChannel = value;
+                        },
                       ),
-                      const SizedBox(
-                        height: 10,
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: TextFormField(
+                        keyboardType: TextInputType.url,
+                        style: TextStyle(
+                          fontFamily: 'FredokaOne',
+                          fontSize: 15,
+                          color: Colors.orange[700],
+                        ),
+                        textAlign: TextAlign.start,
+                        decoration: const InputDecoration(
+                          labelText: 'Enter HTTP Auth',
+                          hintText: 'Thing 7686eb82-d6b5-4513-b559-d867c8ed85e3',
+                          labelStyle: TextStyle(),
+                          hintStyle: TextStyle(),
+                        ),
+                        onChanged: (value) {
+                          _httpAuth = value;
+                        },
                       ),
-                      Container(
-                        height:35,
-                        padding: const EdgeInsets.symmetric(horizontal: 15),
-                        width: MediaQuery.of(context).size.width * 0.95,
-                        child: ElevatedButton(
-                            onPressed: () {
-                              if (formKey.currentState!.validate()) {
-                                UniversalMqttClient? mclient;
-                                Navigator.push(context, MaterialPageRoute(
-                                    builder: (BuildContext context) {
-                                  return JoypadPage(
-                                    endpoint: _endpoint,
-                                    protocol: _protocol,
-                                    mqttClient: mclient,
-                                    mqttUsername: _mqttUsername,
-                                    mqttPassword: _mqttPassword,
-                                    pubChannel: _pubChannel,
-                                    httpAuth: _httpAuth,
-                                  );
-                                }));
-                              }
-                            },
-                            style: TextButton.styleFrom(
-                              foregroundColor: Colors.white,
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(5.0)),
-                              backgroundColor: Colors.orange[700],
-                            ),
-                            child: const Text(
-                              "Setup",
-                              style: TextStyle(color: Colors.white),
-                            )),
-                      )
-                    ],
-                  ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      height:35,
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      width: MediaQuery.of(context).size.width * 0.95,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            if (formKey.currentState!.validate()) {
+                              UniversalMqttClient? mclient;
+                              Navigator.push(context, MaterialPageRoute(
+                                  builder: (BuildContext context) {
+                                return JoypadPage(
+                                  endpoint: _endpoint,
+                                  protocol: _protocol,
+                                  mqttClient: mclient,
+                                  mqttUsername: _mqttUsername,
+                                  mqttPassword: _mqttPassword,
+                                  pubChannel: _pubChannel,
+                                  httpAuth: _httpAuth,
+                                );
+                              }));
+                            }
+                          },
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5.0)),
+                            backgroundColor: Colors.orange[700],
+                          ),
+                          child: const Text(
+                            "Setup",
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    )
+                  ],
                 ),
               ),
             ),
